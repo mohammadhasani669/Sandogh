0bc01e4f7eecee7561a7d4dd9bd869a86d416b4f
mohammadhasani669 <mohammadhasani669@gmail.com>
2022-03-03 09:19:51 +0330

    exception

---
+++ Src/Core/Sandogh.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandogh.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityName, object key)
+             : base($"Entity  {entityName} with key {key} was not found.")
+        {
+
+        }
+    }
+}
